09f4557a69c7d9adf65dd1da5c545285dae020a0
laptoff <laptoff.contact@gmail.com>
2024-02-08 18:44:15 +0100

    update the intro of the plugin

--- src/main/java/fr/laptoff/authplugin/AuthPlugin.java
+++ src/main/java/fr/laptoff/authplugin/AuthPlugin.java
@@ -1,6 +1,13 @@
 package fr.laptoff.authplugin;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.N;
 
 import java.util.logging.Logger;
 
@@ -10,14 +17,36 @@ public final class AuthPlugin extends JavaPlugin {
     public static final Logger LOGGER = Logger.getLogger("AuthPlugin");
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         instance = this;
 
         LOGGER.info("The authentication system started !");
 
-        LOGGER.info(" /|  ");
-        LOGGER.info("/-||)");
-        LOGGER.info("   | ");
+        //Start introduction.
+        Bukkit.getConsoleSender().sendMessage(Component.text("by ")
+                .color(NamedTextColor.YELLOW)
+                .append(Component.text("laptoff,")
+                        .color(NamedTextColor.GOLD)
+                        .decorate(TextDecoration.ITALIC)));
+
+        Bukkit.getConsoleSender().sendMessage(Component.text(""));
+
+        Bukkit.getConsoleSender().sendMessage(Component.text(" /|  ")
+                .color(NamedTextColor.GREEN));
+
+        Bukkit.getConsoleSender().sendMessage(Component.text("/-|")
+                .color(NamedTextColor.GREEN)
+                .append(Component.text("|)")
+                        .color(NamedTextColor.BLUE)));
+
+        Bukkit.getConsoleSender().sendMessage(Component.text("   | ")
+                .color(NamedTextColor.BLUE)
+                .append(Component.text("     Auth")
+                        .color(NamedTextColor.GREEN))
+                .append(Component.text("Plugin")
+                        .color(NamedTextColor.BLUE)));
+
+
     }
 
     @Override
